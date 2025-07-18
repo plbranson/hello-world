cb525aca6cbd6d803f261cb2b3e75436c6877d64
PatrickLBranson <plbranson@icloud.com>
2025-07-17 20:55:24 -0400

    Working project

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ *  Copyright 2025 Patrick L. Branson
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+const cors = require("cors");
+const express = require("express");
+
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {});
+
+app.listen(PORT, (error) => {
+  if (!error) console.log(`Running on http://localhost:${PORT}`);
+  else console.log("Error: Failed To Start The Server!", error);
+});
